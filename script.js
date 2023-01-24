44347d0d7a06b96e61cb3424982ab44250364922
Ananthakrishnan21 <ananthakrishnan430@gmail.com>
2023-01-24 12:26:42 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 function threeSum(arr, target) {
   //your code here
+  arr.sort(function(a,b){return a-b})
+  console.log(arr)
+  let sum=Number.MAX_VALUE
+  let ans=0
+  for(let k=arr.length-1;k>=0;k--){
+    let i=0;
+    let j=i+1
+    while(i<j && j<k){
+      let currentSum=arr[i]+arr[j]+arr[k]
+      if(currentSum==target){
+        return target
+      }
+      else if(currentSum>target){
+        
+          if(currentSum-target<sum){
+          sum=currentSum-target
+          ans=currentSum
+        }
+        break
+      }
+      else{
+        if(target-currentSum<sum){
+          sum=target-currentSum
+          ans=currentSum
+        }
+        j++
+      }
+    }
+  }
+  return sum
 }
 
 module.exports = threeSum;
